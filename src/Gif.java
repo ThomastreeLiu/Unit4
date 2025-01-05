69259381492de92598f2873acb560e5abf703106
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2025-01-05 01:05:00 -0800

    zoo finish

---
+++ src/Gif.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Gif extends JPanel implements ActionListener {
+
+
+    public Gif() {
+        setBackground(Color.cyan);
+        JLabel pic1 = new JLabel(createImageIcon("test.gif"));
+//constructor with initializing code in here
+        add(pic1);
+    }
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = Gif.class.getResource(path);
+
+        if (imgURL != null) {
+
+            return new ImageIcon(imgURL);
+
+        } else {
+
+            System.err.println("Couldn't find file: " + path);
+
+            return null;
+
+        }
+
+    }
+    public static void main(String[] args) {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("GUIBasicN");
+        frame.setSize(470, 350);         //resizes JFrame pane size
+        frame.setBackground(Color.WHITE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Gif panel = new Gif();
+        panel.setOpaque(true);
+        frame.add(panel);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
